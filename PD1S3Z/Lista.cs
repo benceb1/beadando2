62af0c81c8851ad2e3774c8b174e1b517cf6399a
benceb1 <bence.b1.1.2@gmail.com>
2019-04-18 11:53:33 +0200

    elso

---
+++ PD1S3Z/Lista.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD1S3Z
+{
+    class RendezettLista<T> where T : ILejatszhato
+    {
+        class ListaElem
+        {
+            public ListaElem kovetkezo;
+            public T tartalom;
+            public Kulcs kulcs;
+        }
+        private ListaElem ujElem(T tartalom)
+        {
+            return new ListaElem()
+            { tartalom = tartalom,
+                kulcs = new Kulcs(tartalom.Stilus)
+            };
+        }
+    }
+    class Kulcs
+    {
+        Stilus stilus { get; set; }
+        public Kulcs(Stilus stilus)
+        {
+            this.stilus = stilus;
+        }
+        public static bool operator <(Kulcs kulcs1, Kulcs kulcs2)
+        {
+            if ((int)kulcs1.stilus < (int)kulcs2.stilus)
+                return true;
+            return false;
+        }
+        public static bool operator >(Kulcs kulcs1, Kulcs kulcs2)
+        {
+            if ((int)kulcs1.stilus > (int)kulcs2.stilus)
+                return true;
+            return false;
+        }
+    }
+}
